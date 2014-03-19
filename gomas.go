0f6ee4735dfa5be5d1248dddb662c556aeee2ce4
Leif Walsh <leif.walsh@gmail.com>
2014-03-19 15:54:53 -0400

    added initial gomas.go implementation

---
+++ gomas.go
@@ -0,0 +1,283 @@
+package main
+
+import (
+	"bufio"
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"github.com/bmizerany/perks/quantile"
+	"io"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	timeHandlers = 1  // threads pulling from the web, unzipping, chopping into lines (beware webserver throttle)
+	lineHandlers = 8  // threads parsing lines into bson.M objects
+	docHandlers  = 32 // threads pushing into mongo
+)
+
+func codeToProject(code string) (project string, err error) {
+	switch code {
+	case "b":
+		project = "wikibooks"
+	case "d":
+		project = "wiktionary"
+	case "m":
+		project = "wikimedia"
+	case "mw":
+		project = "wikipedia mobile"
+	case "n":
+		project = "wikinews"
+	case "q":
+		project = "wikiquote"
+	case "s":
+		project = "wikisource"
+	case "v":
+		project = "wikiversity"
+	case "w":
+		project = "mediawiki"
+	default:
+		err = errors.New("unknown project")
+	}
+	return
+}
+
+type RawRecord struct {
+	line string
+	time time.Time
+}
+
+func parseLine(rec RawRecord) (entry bson.M, err error) {
+	doc := make(bson.M)
+	parts := strings.Split(rec.line, " ")
+	siteparts := strings.Split(parts[0], ".")
+	doc["lang"] = siteparts[0]
+	doc["project"] = "wikipedia"
+	if len(siteparts) > 1 {
+		if doc["project"], err = codeToProject(siteparts[1]); err != nil {
+			return
+		}
+	}
+	unesc, err := url.QueryUnescape(parts[1])
+	if err != nil {
+		//log.Println("Error decoding", rec.line, ":", err)
+		doc["url"] = parts[1]
+		doc["malformed"] = true
+	} else {
+		doc["url"] = unesc
+	}
+	doc["requests"], err = strconv.Atoi(parts[2])
+	if err != nil {
+		return
+	}
+	doc["size"], err = strconv.Atoi(parts[3])
+	if err != nil {
+		return
+	}
+	doc["time"] = rec.time
+	entry = doc
+	return
+}
+
+func retryInsert(coll *mgo.Collection, batch ...interface{}) (err error) {
+	for err := coll.Insert(batch...); err != nil; err = coll.Insert(batch...) {
+		if lerr, ok := err.(*mgo.LastError); !ok || lerr.Code != 16759 {
+			break
+		}
+		time.Sleep(time.Millisecond)
+		log.Println("Retrying insert due to lock not granted")
+	}
+	return
+}
+
+func main() {
+	sess, err := mgo.Dial("localhost")
+	if err != nil {
+		log.Fatal("Error connecting to mongo: ", err)
+	}
+	defer sess.Close()
+	//sess.SetSafe(nil) //&mgo.Safe{WMode: "majority"})
+	coll := sess.DB("wiki").C("rawlogs")
+	if err := coll.DropCollection(); err != nil {
+		log.Println(err)
+	}
+
+	var (
+		linesDone            = sync.WaitGroup{}
+		lines                = make(chan RawRecord, 1000)
+		docsDone             = sync.WaitGroup{}
+		docs                 = make(chan bson.M, 5000)
+		insertCounter uint64 = 0
+		kill                 = make(chan bool)
+		cq                   = quantile.NewTargeted(0.50, 0.90, 0.99)
+		wq                   = quantile.NewTargeted(0.50, 0.90, 0.99)
+		latencies            = make(chan time.Duration)
+	)
+	go func() {
+		for t := range latencies {
+			var v float64 = float64(t) / float64(time.Millisecond)
+			cq.Insert(v)
+			wq.Insert(v)
+		}
+	}()
+	go func() {
+		start := time.Now()
+		ticks := time.NewTicker(time.Second)
+		_, ok := <-ticks.C
+		last := start
+		var lastCount uint64 = 0
+		var linesPrinted int = 0
+		for ok {
+			thisCount := atomic.LoadUint64(&insertCounter)
+			diff := thisCount - lastCount
+			now := time.Now()
+			idur := now.Sub(last)
+			cdur := now.Sub(start)
+			if linesPrinted%24 == 0 {
+				log.Printf("-- docs -- -------- ips ------- ------- window latency ------- ----- cumulative latency ----\n")
+				log.Printf("    total |     iter       cum |      50%%       90%%       99%% |      50%%       90%%       99%%\n")
+			}
+			linesPrinted++
+			log.Printf("%9d |%9.2f %9.2f |%9.3f %9.3f %9.3f |%9.3f %9.3f %9.3f\n", thisCount, float64(diff)/idur.Seconds(), float64(thisCount)/cdur.Seconds(), wq.Query(0.50), wq.Query(0.90), wq.Query(0.99), cq.Query(0.50), cq.Query(0.90), cq.Query(0.99))
+			wq.Reset()
+			select {
+			case _, ok = <-ticks.C:
+				lastCount = thisCount
+				last = now
+			case <-kill:
+				break
+			}
+		}
+	}()
+	for i := 0; i < lineHandlers; i++ {
+		linesDone.Add(1)
+		go func() {
+			for rec := range lines {
+				doc, err := parseLine(rec)
+				if err != nil {
+					log.Fatal("Error parsing ", rec.line, ": ", err)
+				} else {
+					docs <- doc
+				}
+			}
+
+			linesDone.Done()
+		}()
+	}
+	for i := 0; i < docHandlers; i++ {
+		docsDone.Add(1)
+		go func() {
+			workerSession := sess.New()
+			defer workerSession.Close()
+			coll := workerSession.DB("wiki").C("rawlogs")
+			batch := make([]interface{}, 0, 100)
+			for doc := range docs {
+				batch = append(batch, doc)
+				if len(batch) == cap(batch) {
+					t0 := time.Now()
+					if err := retryInsert(coll, batch...); err != nil {
+						log.Fatal("Error inserting batch: ", err)
+					}
+					atomic.AddUint64(&insertCounter, uint64(len(batch)))
+					latencies <- time.Since(t0)
+					batch = batch[:0]
+				}
+			}
+			if len(batch) > 0 {
+				t0 := time.Now()
+				if err := retryInsert(coll, batch...); err != nil {
+					log.Fatal("Error inserting batch: ", err)
+				}
+				atomic.AddUint64(&insertCounter, uint64(len(batch)))
+				latencies <- time.Since(t0)
+			}
+			docsDone.Done()
+		}()
+	}
+
+	var (
+		timesDone = sync.WaitGroup{}
+		times     = make(chan time.Time, 10)
+	)
+	for i := 0; i < timeHandlers; i++ {
+		timesDone.Add(1)
+		go func() {
+			for t := range times {
+				filename := fmt.Sprintf("http://dumps.wikimedia.org/other/pagecounts-raw/%04d/%04d-%02d/pagecounts-%04d%02d%02d-%02d0000.gz", t.Year(), t.Year(), t.Month(), t.Year(), t.Month(), t.Day(), t.Hour())
+				resp, err := http.Get(filename)
+				if err != nil {
+					if err == io.EOF || strings.HasSuffix(err.Error(), "EOF") {
+						log.Println("EOF getting file ", filename)
+						times <- t
+						time.Sleep(time.Second)
+						continue
+					}
+					log.Fatal("Error getting file ", filename, ": ", err)
+				}
+				if resp.StatusCode == 404 {
+					resp.Body.Close()
+					filename = fmt.Sprintf("http://dumps.wikimedia.org/other/pagecounts-raw/%04d/%04d-%02d/pagecounts-%04d%02d%02d-%02d0001.gz", t.Year(), t.Year(), t.Month(), t.Year(), t.Month(), t.Day(), t.Hour())
+					resp, err = http.Get(filename)
+					if err != nil {
+						if err == io.EOF || strings.HasSuffix(err.Error(), "EOF") {
+							log.Println("EOF getting file ", filename)
+							times <- t
+							time.Sleep(time.Second)
+							continue
+						}
+						log.Fatal("Error getting file ", filename, ": ", err)
+					}
+				}
+				defer resp.Body.Close()
+
+				if resp.StatusCode != 200 {
+					log.Println("HTTP status", resp.StatusCode, "while getting", filename)
+					continue
+				}
+
+				raw, err := gzip.NewReader(resp.Body)
+				if err != nil {
+					if err == io.EOF {
+						log.Println("EOF unzipping file ", filename)
+						continue
+					}
+					log.Fatal("Error unzipping file ", filename, ": ", err)
+				}
+				defer raw.Close()
+
+				sc := bufio.NewScanner(raw)
+				for sc.Scan() {
+					lines <- RawRecord{sc.Text(), t}
+				}
+				if err := sc.Err(); err != nil {
+					log.Fatal("Error reading from Scanner for file ", filename, ": ", err)
+				}
+			}
+
+			timesDone.Done()
+		}()
+	}
+
+	var twoThousandFourteen = time.Date(2014, time.January, 1, 0, 0, 0, 0, time.UTC)
+	for date := time.Date(2008, time.January, 1, 0, 0, 0, 0, time.UTC); date.Before(twoThousandFourteen); date = date.Add(time.Hour) {
+		times <- date
+	}
+
+	close(times)
+	timesDone.Wait()
+	close(lines)
+	linesDone.Wait()
+	close(docs)
+	docsDone.Wait()
+	close(latencies)
+	kill <- true
+}
